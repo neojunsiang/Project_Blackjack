8b0e1c45e08d2642ecbd7f81d57d306fee47c6d8
imjunsiang <neojunsiang@gmail.com>
2021-03-07 17:43:51 +0800

    completed game logic

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,8 +1,8 @@
 // console.log("Linked");
 
 const deck = [];
-const suit = ["Hearts", "Spades"];
-const value = ["K", 10];
+// const suit = ["Hearts", "Spades"];
+// const value = ["K", 10, 2];
 const player1 = {
     card: [],
     cardOnHand: 0,
@@ -14,33 +14,10 @@ const dealer = {
     whoseTurn: false
 };
 
-// const suit = ["Hearts", "Spades", "Diamonds", "Clubs"];
-// const value = ["K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2, "A"];
+const suit = ["Hearts", "Spades", "Diamonds", "Clubs"];
+const value = ["K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2, "A"];
 
-// const card1 = {
-//     suit: suit[0],
-//     value: value[0]
-// };
-// console.log(card1);
-
-// const card2 = {
-//     suit: suit[0],
-//     value: value[1]
-// }
-// console.log(card2);
-
-// const card3 = {
-//     suit: suit[1],
-//     value: value[0]
-// }
-// console.log(card3);
-
-// const card4 = {
-//     suit: suit[1],
-//     value: value[1]
-// }
-// console.log(card4);
-
+// Make Cards
 const makeCard = (suit, value) => {
     for (let i = 0; i < suit.length; i++) {
         for (let j = 0; j < value.length; j++) {
@@ -54,24 +31,9 @@ const makeCard = (suit, value) => {
     }
 }
 
-makeCard(suit, value);
-// console.log(deck);
-// console.log(deck.length);
+// makeCard(suit, value);
 
-// let cardToShuffle = deck[2];
-// console.log("deck[2]", cardToShuffle);
-// deck[2] = deck[0];
-// console.log(deck);
-// deck[0] = cardToShuffle;
-// console.log(deck);
-
-// let cardToShuffle2 = deck[1];
-// console.log("deck[1]", cardToShuffle2);
-// deck[1] = deck[3];
-// console.log(deck);
-// deck[3] = cardToShuffle2;
-// console.log(deck);
-
+// Shuffle Cards
 const shuffleCard = () => {
     for (let i = 0; i < deck.length; i++) {
         let location1 = Math.floor(Math.random() * (deck.length - 1));
@@ -87,31 +49,9 @@ const shuffleCard = () => {
         // console.log(deck);
     }
 }
-
-shuffleCard();
-
+// shuffleCard();
 
 // Distribute cards
-
-// console.log("deck Before", deck);
-// console.log("player1 Before", player1);
-// console.log("dealer Before", dealer);
-// player1.push(deck[0]);
-// deck.shift();
-// console.log("deck After", deck);
-// console.log("player1 After", player1);
-// dealer.push(deck[1]);
-// deck.shift();
-// console.log("deck After After", deck);
-// console.log("dealer", dealer);
-// player1.push(deck[0]);
-// deck.shift();
-// console.log("deck After", deck);
-// console.log("player1 After", player1);
-// dealer.push(deck[1]);
-// deck.shift();
-// console.log("deck After After", deck);
-
 const distributeCards = () => {
     for (let i = 0; i < 2; i++) {
         player1.card.push(deck[0]);
@@ -119,19 +59,9 @@ const distributeCards = () => {
         dealer.card.push(deck[1]);
         dealer.cardOnHand++;
         deck.splice(0, 2);
-        // deck.shift();
-        // console.log("player1", player1);
-        // console.log("dealer", dealer);
-        // console.log("deck", deck);
     }
-    console.log("player1 hands", player1.cardOnHand);
-    console.log("dealer hands", dealer.cardOnHand);
 }
-distributeCards();
-
-// console.log(player1);
-// console.log("initial 1", player1.card[0].value);
-// console.log("initial 2", player1.card[1].value);
+// distributeCards();
 
 const countPoints = (input) => {
     let total = 0;
@@ -139,63 +69,110 @@ const countPoints = (input) => {
         for (let i = 0; i < input.card.length; i++) {
             if (input.card[i].value === "A") {
                 input.card[i].value = 11;
-                console.log("new", input.card[i].value);
+                // console.log("new", input.card[i].value);
             } else if (input.card[i].value === "K" || input.card[i].value === "Q" || input.card[i].value === "J") {
                 input.card[i].value = 10;
-                console.log("non-A value", input.card[i].value);
+                // console.log("non-A value", input.card[i].value);
             } else {
-                console.log("no change", input.card[i].value);
+                input.card[i].value;
+                // console.log("no change", input.card[i].value);
             }
             total += input.card[i].value;
         }
-        return total;
+        return input.points = total;
     } else if (input.cardOnHand === 3) {
         for (let i = 0; i < input.card.length; i++) {
             if (input.card[i].value === "A") {
                 input.card[i].value = 10;
-                console.log("new", input.card[i].value);
+                // console.log("new", input.card[i].value);
             } else if (input.card[i].value === "K" || input.card[i].value === "Q" || input.card[i].value === "J") {
                 input.card[i].value = 10;
-                console.log("non-A value", input.card[i].value);
+                // console.log("non-A value", input.card[i].value);
             } else {
-                console.log("no change", input.card[i].value);
+                input.card[i].value;
+                // console.log("no change", input.card[i].value);
             }
             total += input.card[i].value;
         }
-        return total;
+        return input.points = total;
     } else if (input.cardOnHand >= 4) {
         for (let i = 0; i < input.card.length; i++) {
             if (input.card[i].value === "A") {
                 input.card[i].value = 1;
-                console.log("new", input.card[i].value);
+                // console.log("new", input.card[i].value);
             } else if (input.card[i].value === "K" || input.card[i].value === "Q" || input.card[i].value === "J") {
                 input.card[i].value = 10;
-                console.log("non-A value", input.card[i].value);
+                // console.log("non-A value", input.card[i].value);
             } else {
-                console.log("no change", input.card[i].value);
+                input.card[i].value;
+                // console.log("no change", input.card[i].value);
             }
             total += input.card[i].value;
         }
-        return total;
+        return input.points = total;
     }
 }
+// console.log(player1.card);
+// console.log(countPoints(player1));
+
+// const changeTurn = (who) => {
+//     console.log(countPoints(who));
+//     if (who.whoseTurn === true) {
+//         return dealer
+//     } else {
+//         return player1
+//     }
+// }
+// console.log(changeTurn(player1));
+
+makeCard(suit, value);
+// console.log("initial", deck);
+shuffleCard();
+// console.log("after shuffle", deck);
+distributeCards();
+// console.log("initial on player", player1);
+// console.log("initial on dealer", dealer);
+countPoints(player1);
+countPoints(dealer);
+console.log("after point count player1", player1); {
+    console.log("after point count dealer", dealer);
+}
+console.log("deck after distribution", deck);
+
+// console.log(deck.shift());
+// player1.card.push(deck.shift());
+// player1.cardOnHand++;
+// countPoints(player1);
 // console.log(player1);
-// console.log("count points player", countPoints(player1));
-// console.log("count points dealer", countPoints(dealer));
-// console.log("count points player", countPoints(player1));
-// console.log("count points dealer", countPoints(dealer));
-// console.log("count points player", countPoints(player1));
-// console.log("count points dealer", countPoints(dealer));
+// console.log("deck after first draw", deck)
 
-const changeTurn = (who) => {
-    countPoints(who);
-    if (!who.whoseTurn) {
-        return dealer
+const hitCard = (userInput) => {
+    userInput.card.push(deck.shift());
+    userInput.cardOnHand++;
+    countPoints(userInput);
+    // console.log("deck length", deck.length);
+    return userInput;
+}
+hitCard(player1);
+console.log("player deck after first hit", player1);
+console.log("dealer deck after first hit", dealer);
+console.log('deck after first hit', deck);
+
+console.log(player1.points);
+
+const checkWin = () => {
+    if (player1.points > 21) {
+        return "Player Bust!";
+    } else if (dealer.points > 22) {
+        return "Dealer Bust!"
     } else {
-        return player1
+        if (player1.points > dealer.points) {
+            return "Player Wins!"
+        } else if (player1.points < dealer.points) {
+            return "Dealer Wins!"
+        } else {
+            return "Game Tie!"
+        }
     }
 }
-console.log(changeTurn(player1));
-
-
-
+console.log(checkWin());
